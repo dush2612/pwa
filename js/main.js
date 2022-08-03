acc804394fd5817e7a665ef2513dbccad893e100
dush2612 <109569945+dush2612@users.noreply.github.com>
2022-08-03 22:20:13 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,23 @@
+// populate install dialog in mobile devices
+var deferredPrompt;
+
+if (!window.Promise) {
+  window.Promise = Promise;
+}
+
+// register service worker
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("/sw.js")
+    .then((reg) => console.log("service worker registered", reg))
+    .catch((err) => console.log("service worker not registered", err));
+}
+
+// fire install button in mobile device
+
+window.addEventListener("beforeinstallprompt", function (event) {
+  console.log("beforeinstallprompt fired");
+  event.preventDefault();
+  deferredPrompt = event;
+  return false;
+});
