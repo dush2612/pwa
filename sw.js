f5e2dccebba53b58afa0b5d54d43cdd4b0adc54e
dush2612 <109569945+dush2612@users.noreply.github.com>
2022-08-03 22:18:49 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,58 @@
+// Welcome to MouriTech PWA Snippet
+
+const staticCacheName = "site-static-v2";
+const dynamicCacheName = "site-dynamic-v1";
+
+// configure static resource here (html, routes, images, urls etc.)
+const assets = ["/", "/index.html"];
+
+const self = this;
+
+// install event
+self.addEventListener("install", (evt) => {
+  //console.log(`service worker activated`);
+  evt.waitUntil(
+    caches.open(staticCacheName).then((cache) => {
+      cache.addAll(assets);
+    })
+  );
+});
+
+// activate event
+self.addEventListener("activate", (evt) => {
+  //console.log(`service worker activated`);
+  evt.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys
+          .filter((key) => key !== staticCacheName && key !== dynamicCacheName)
+          .map((key) => caches.delete(key))
+      );
+    })
+  );
+});
+
+// fetch event (static & dynamic => A cache with network fall back.)
+self.addEventListener("fetch", (evt) => {
+  //console.log('fetch event', evt);
+  evt.respondWith(
+    caches
+      .match(evt.request)
+      .then((cacheRes) => {
+        return (
+          cacheRes ||
+          fetch(evt.request).then((fetchRes) => {
+            return caches.open(dynamicCacheName).then((cache) => {
+              cache.put(evt.request.url, fetchRes.clone());
+              return fetchRes;
+            });
+          })
+        );
+      })
+      .catch(() => {
+        if (evt.request.url.indexOf(".html") > -1) {
+          return caches.match("/fallback.html");
+        }
+      })
+  );
+});
