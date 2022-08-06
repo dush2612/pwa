f2b2160a424b749c6dd1924ce0eb3054a9e92d0e
dush2612 <109569945+dush2612@users.noreply.github.com>
2022-08-06 21:58:10 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -7,19 +7,4 @@ if ("serviceWorker" in navigator) {
     .then((reg) => console.log("service worker registered", reg))
     .catch((err) => console.log("service worker not registered", err));
 }
-
-// fire install button in mobile device
-
- // This variable will save the event for later use.
-let deferredPrompt;
-window.addEventListener('beforeinstallprompt', (e) => {
-  // Prevents the default mini-infobar or install dialog from appearing on mobile
-  e.preventDefault();
-  // Save the event because you'll need to trigger it later.
-  deferredPrompt = e;
-  // Show your customized install prompt for your PWA
-  // Your own UI doesn't have to be a single element, you
-  // can have buttons in different locations, or wait to prompt
-  // as part of a critical journey.
-  showInAppInstallPromotion();
-});
+ 
